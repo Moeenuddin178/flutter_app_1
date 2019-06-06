fdd2989e91889ea30321f3f01df4f171c0e3c5e8
Moeenuddin <moeenuddin178@gmail.com>
2019-06-06 09:31:45 +0500

    just adding info of the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+//app start running from main
 void main() {
+  //to diplay anything we need a views in android but here we need widgets
   //material app is widget
   runApp(MaterialApp(
     //home and center are also widgets
