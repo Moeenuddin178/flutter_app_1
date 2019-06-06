f590ccd332b8c2a0e21f1aef9849842a6ad9865e
Moeenuddin <moeenuddin178@gmail.com>
2019-06-06 11:27:09 +0500

    image at centre

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,20 @@ void main() {
   runApp(MaterialApp(
     //home and center are also widgets
     //so in home we start rendring
-    home: Center(
-      child: Text('hi'),
+    home: Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        title: Text('I am witch'),
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Center(
+        child: Image(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')),
+      ),
     ),
   ));
 }
+
+//so far tree is form parent is material app  which materilize thing
+//then home widget
