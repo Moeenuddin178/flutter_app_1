3e4118ad8c1e51bd4314cba78e99c3ec85e005fa
Moeenuddin <moeenuddin178@gmail.com>
2019-06-06 09:21:44 +0500

    This is just hello world at center of moblie screen

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //material app is widget
+  runApp(MaterialApp(
+    //home and center are also widgets
+    home: Center(
+      child: Text('hi'),
+    ),
+  ));
+}
